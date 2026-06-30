b0612ee4fd7b3c4d13d507e29e46a0bf2a0a73f1
thanks-cohn <officialmahasbiz@gmail.com>
2026-06-30 01:39:58 -0600

    Create page.js

---
+++ src/page/page.js
@@ -0,0 +1,24 @@
+// src/page/page.js
+
+import { Landing } from "./landing.js";
+import { Reader } from "./reader.js";
+
+export class Page {
+
+    static async start() {
+
+        const params = new URLSearchParams(window.location.search);
+
+        const work = params.get("work");
+        const chapter = params.get("chapter");
+
+        if (work && chapter) {
+            await Reader.start(work, chapter);
+            return;
+        }
+
+        await Landing.start();
+
+    }
+
+}
